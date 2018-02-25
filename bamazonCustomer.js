e3ceb9c89dd625dbcdd4a8216197cd592e4bc3cd
Soraia <Soraia@So-MacBook-Pro.local>
2018-02-24 21:26:55 -0500

    second

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,88 +21,83 @@ connection.connect(function(err) {
 var displayItems = function(){
   var query = "SELECT * FROM products";
   connection.query(query, function(err, res) {
-      for (var i = 0; i < res.length; i++) {
-          console.log("Item ID: " + res[i].item_id + " || Product: " + res[i].product_name + " || Department: " + res[i].department_name + " || Price: " + res[i].price + " || Stock: " + res[i].stock_qty);
-      }
-      shoppingCart();
-    })
+    for (var i = 0; i < res.length; i++) {
+      console.log("Item ID: " + res[i].item_id + " || Product: " + res[i].product_name + " || Department: " + res[i].department_name + " || Price: " + res[i].price + " || Stock: " + res[i].stock_qty);
+    }
+    shoppingCart();
+  })
 };
 //Users prompted with two questioons:
 //1.the ID of the product they would like to buy
 //2.how many untis they would like to buy
 var shoppingCart = function() {
-    inquirer.prompt([
-      {
-        name: "itemID",
-        type: "input",
-        message: "What is the ID of the product you would like to buy?",
-        //Validate: checks weather or not the user typed a response
-        validate: function(value) {
-            if (isNaN(value) == false) {
-                return true;
-            } else {
-                console.log("\nPlease enter a valid product ID");
-                return false;
-            }
-        }
-      },
-      {
-        name: "quantity",
-        type: "input",
-        message: "How many units of this product you would like to buy?",
-        validate: function(value) {
-            if (isNaN(value) == false) {
-                return true;
-            } else {
-                return false;
-            }
+  inquirer.prompt([
+    {
+      name: "itemID",
+      type: "input",
+      message: "What is the ID of the product you would like to buy?",
+      //Validate: checks weather or not the user typed a response
+      validate: function(value) {
+        if (isNaN(value) == false) {
+          return true;
+        } else {
+          console.log("\nPlease enter a valid product ID");
+          return false;
         }
       }
-    ])
-//When finished prompting:
-.then(function(answer) {
-//Check if store has enough quantity of the product to meet the customer's request.
-//If not: "Insufficient quantity"
-var query = 'SELECT * FROM products WHERE item_id=' + answer.itemID;
+    },
+    {
+      name: "quantity",
+      type: "input",
+      message: "How many units of this product you would like to buy?",
+      validate: function(value) {
+        if (isNaN(value) == false) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  ])
+  //When finished prompting:
+  .then(function(answer) {
+    //Check if store has enough quantity of the product to meet the customer's request.
+    //If not: "Insufficient quantity"
+    var query = 'SELECT * FROM products WHERE item_id=' + answer.itemID;
     connection.query(query, function(err, res) {
       console.log("res",res);
       console.log("answer.quantity: ",answer.quantity);
       console.log("res.stock_qty",res[0].stock_qty);
       if (answer.quantity <= res[0].stock_qty) {
-          for (var i = 0; i < res.length; i++) {
+        for (var i = 0; i < res.length; i++) {
           console.log("We currently have " + res[i].stock_qty + " " + res[i].product_name + ".");
+          console.log("Your total is " + res[0].Price * answer.quantity);
           console.log("Thank you! Your order of "+ answer.quantity + " " + res[i].product_name + " is now being processed.");
-// ??     // update the items
+          // update the items
           var newQuantity = res[i].stock_qty - answer.quantity;
-          function updateItem(answer.itemID,newQuantity) {
-          var query = connection.query("UPDATE products SET ? WHERE ?",
-          [{
-            item_id = answer.itemID;
+          var newAnswer = answer.itemID;
+          function updateItem(newAnswer, newQuantity) {
+            var query = connection.query("UPDATE products SET ? WHERE ?",
+            [{
+              stock_qty:res[0].stock_qty - answer.quantity,
             },
-          {
-            stock_qty = answer.quantity;
-            }
-          ]
-          function(err, res) {
-          console.log(res.newQuantity + " product updated!\n");
-          // Call updateItem AFTER the UPDATE completes
-//          updateItem();
+            {
+              item_id: newAnswer,
+            }],
+            function(err, res) {
+              console.log(res.newQuantity);
+              // Call updateItem AFTER the UPDATE completes
+              //updateItem();
+            })
           }
-        }
           updateItem();
-        }
-// ????
-//if qty is less that the stock_qty
+        };
+        //if qty is less that the stock_qty
       } else {
-          console.log("Sorry! We have insufficient quantity of this product in stock.");
-          }
-        //displayItems();
-      })
+        console.log("Sorry! We have insufficient quantity of this product in stock.");
+      }
+      displayItems();
     })
- };
-//  //missing total cost of purchase????
-
-
-
-//    // logs the actual query being run
-//    console.log(query.sql);
+  });
+};
+  //  //missing total cost of purchase????
